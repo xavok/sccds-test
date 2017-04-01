05d944463f8338f9ebf7bdbb95fa8f1b032306d3
Max Nasyrov <max.nasyrov@foobl.com>
2017-03-31 17:35:54 -0700

    Added profile page to see users info

--- author.php
+++ author.php
@@ -1,12 +1,7 @@
 <?php
-/*
-Template Name: User Profile
-*/
-if (!is_user_logged_in()) {
-    auth_redirect();
-}
+
 get_header();
-$current_user = wp_get_current_user();
+$author = get_queried_object();
 $args = array(
     'post_type' => 'job_application',
     'post_status' => ['interviewed', 'offer', 'new']
@@ -14,7 +9,7 @@ $args = array(
 $user_application = get_posts($args);
 $jobs = [];
 foreach ($user_application as $job) {
-    if (get_post_meta($job->ID, '_candidate_user_id', true) == $current_user->ID) {
+    if (get_post_meta($job->ID, '_candidate_user_id', true) == $author->ID) {
         $jobs[] = $job->post_parent;
     };
 }
@@ -27,23 +22,22 @@ if (!empty($jobs)) {
 } else {
     $jobs_applied = [];
 }
-
-
 ?>
 
     <div id="page-full-width" role="main">
 
         <?php do_action('foundationpress_before_content'); ?>
-        <?php while (have_posts()) : the_post(); ?>
             <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-                <?php echo get_avatar($current_user->ID); ?>
-                <h1><?php echo $current_user->user_login . '<br />'; ?></h1>
+                <?php echo get_avatar($author->ID); ?>
+                <h1><?php echo $author->user_login . '<br />'; ?></h1>
                 <ul>
-                    <li><a href="/edit-profile/">Edit Profile</a></li>
-                    <li><a href="/forums/user/<?php echo $current_user->user_login; ?>/">Check Your Forum</a></li>
+                    <?php if (!is_user_logged_in()) { ?>
+                        <li><a href="/edit-profile/">Edit Profile</a></li>
+                    <?php } ?>
+
+                    <li><a href="/forums/user/<?php echo $author->user_login; ?>/">Forum</a></li>
                     <li><a href="/resources/classifieds/">Add/Edit ads</a></li>
                 </ul>
-                <?php echo do_shortcode('[job_dashboard]'); ?>
                 <h3>Jobs Applied to</h3>
                 <ul>
                     <?php foreach ($jobs_applied as $job) {
@@ -53,7 +47,6 @@ if (!empty($jobs)) {
                 <h3>Courses Enrolled in</h3>
                 <?php echo do_shortcode('[ld_course_list mycourses=’1′]'); ?>
             </article>
-        <?php endwhile; ?>
 
 
 
